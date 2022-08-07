a4d5b3711017b27fa3a3ceb39cab9381ed7d9a97
Ukenn <duola2002@gmail.com>
2022-08-07 12:54:20 +0900

    ⭐ 增加对日本验证码短信的识别

--- MacCopier/AppDelegate.swift
+++ MacCopier/AppDelegate.swift
@@ -160,7 +160,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         let message = getLatestMessage()
         if message != nil {
             let msgText = message!.text
-            if msgText.contains("code") || msgText.contains("码") || msgText.contains("コード") {
+            if msgText.contains("code") || msgText.contains("码") || msgText.contains("コード") || msgText.contains("認証番号") {
                 do {
                     let re = try NSRegularExpression(pattern: codePattern, options: [])
                     let results = re.matches(in: msgText, range: NSRange(location: 0, length: msgText.count))
